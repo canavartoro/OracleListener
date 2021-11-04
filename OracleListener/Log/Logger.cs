17818fb292debdddbeb6911c2af33e0f475d3c51
Hüseyin Çelik <canavar.toro@gmail.com>
2021-11-05 00:28:24 +0300

    calisma

---
+++ OracleListener/Log/Logger.cs
@@ -0,0 +1,140 @@
+﻿using OracleListener.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OracleListener.Log
+{
+    public class Logger
+    {
+        #region Write
+        [DebuggerStepThrough()]
+        public static void Write(Exception exception)
+        {
+            Trace.WriteLine(string.Concat("Trace :: ", DateTime.Now.ToString(), ", Exception: ", exception.Message, ", StackTrace:", exception.StackTrace));
+        }
+        [DebuggerStepThrough()]
+        public static void Write(string str)
+        {
+            Trace.WriteLine(string.Concat("Trace :: ", DateTime.Now.ToString(), ", Mesaj: ", str));
+        }
+        [DebuggerStepThrough()]
+        public static void Write(object obj)
+        {
+            Trace.WriteLine(string.Concat("Trace :: ", DateTime.Now.ToString(), ", Object: ", obj != null ? obj.ToString() : "null"));
+        }
+        #endregion
+
+        #region Verbose
+        [DebuggerStepThrough()]
+        public static void V(Exception exception)
+        {
+            if (AppConfig.Default.TraceLevel >= 4)
+            {
+                Debug.WriteLine(string.Concat("Verbose :: ", DateTime.Now.ToString(), ", Exception: ", exception.Message, ", StackTrace:", exception.StackTrace));
+            }
+        }
+        [DebuggerStepThrough()]
+        public static void V(string str)
+        {
+            if (AppConfig.Default.TraceLevel >= 4)
+            {
+                Trace.WriteLine(string.Concat("Verbose :: ", DateTime.Now.ToString(), ", Mesaj: ", str));
+            }
+        }
+        [DebuggerStepThrough()]
+        public static void V(object obj)
+        {
+            if (AppConfig.Default.TraceLevel >= 4)
+            {
+                Trace.WriteLine(string.Concat("Verbose :: ", DateTime.Now.ToString(), ", Object: ", obj != null ? obj.ToString() : "null"));
+            }
+        }
+        #endregion
+
+        #region Info
+        [DebuggerStepThrough()]
+        public static void I(Exception exception)
+        {
+            if (AppConfig.Default.TraceLevel >= 3)
+            {
+                Trace.WriteLine(string.Concat("Info :: ", DateTime.Now.ToString(), ", Exception: ", exception.Message, ", StackTrace:", exception.StackTrace));
+            }
+        }
+        [DebuggerStepThrough()]
+        public static void I(string str)
+        {
+            if (AppConfig.Default.TraceLevel >= 3)
+            {
+                Trace.WriteLine(string.Concat("Info :: ", DateTime.Now.ToString(), ", Mesaj: ", str));
+            }
+        }
+        [DebuggerStepThrough()]
+        public static void I(object obj)
+        {
+            if (AppConfig.Default.TraceLevel >= 3)
+            {
+                Trace.WriteLine(string.Concat("Info :: ", DateTime.Now.ToString(), ", Object: ", obj != null ? obj.ToString() : "null"));
+            }
+        }
+        #endregion
+
+        #region Warning
+        [DebuggerStepThrough()]
+        public static void W(Exception exception)
+        {
+            if (AppConfig.Default.TraceLevel >= 2)
+            {
+                Trace.WriteLine(string.Concat("Warning :: ", DateTime.Now.ToString(), ", Exception: ", exception.Message, ", StackTrace:", exception.StackTrace));
+            }
+        }
+        [DebuggerStepThrough()]
+        public static void W(string str)
+        {
+            if (AppConfig.Default.TraceLevel >= 2)
+            {
+                Trace.WriteLine(string.Concat("Warning :: ", DateTime.Now.ToString(), ", Mesaj: ", str));
+            }
+        }
+        [DebuggerStepThrough()]
+        public static void W(object obj)
+        {
+            if (AppConfig.Default.TraceLevel >= 2)
+            {
+                Trace.WriteLine(string.Concat("Warning :: ", DateTime.Now.ToString(), ", Object: ", obj != null ? obj.ToString() : "null"));
+            }
+        }
+        #endregion
+
+        #region Error
+        [DebuggerStepThrough()]
+        public static void E(Exception exception)
+        {
+            if (AppConfig.Default.TraceLevel >= 1)
+            {
+                Debug.WriteLine(string.Concat("Error :: ", DateTime.Now.ToString(), ", Exception: ", exception.Message, ", StackTrace:", exception.StackTrace));
+            }
+
+        }
+        [DebuggerStepThrough()]
+        public static void E(string str)
+        {
+            if (AppConfig.Default.TraceLevel >= 1)
+            {
+                Trace.WriteLine(string.Concat("Error :: ", DateTime.Now.ToString(), ", Mesaj: ", str));
+            }
+        }
+        [DebuggerStepThrough()]
+        public static void E(object obj)
+        {
+            if (AppConfig.Default.TraceLevel >= 1)
+            {
+                Trace.WriteLine(string.Concat("Error :: ", DateTime.Now.ToString(), ", Object: ", obj != null ? obj.ToString() : "null"));
+            }
+        }
+        #endregion
+    }
+}
---
+++ OracleListener/Log/TextWriterTraceListener.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OracleListener.Log
+{
+    public class TextWriterTraceListener : TraceListener
+    {
+        private readonly StreamWriter Writer;
+
+        public TextWriterTraceListener()
+        {
+            try
+            {
+                string trace = $"{Application.StartupPath}\\app_trace.txt";
+                FileInfo fi = new FileInfo(trace);
+                bool append = fi.Exists && fi.Length < 1000000;
+                Writer = new StreamWriter(trace, append, Encoding.GetEncoding("windows-1254"));
+                Writer.AutoFlush = true;
+            }
+            catch (Exception exc)
+            {
+                Logger.E(exc);
+            }
+        }
+
+        ~TextWriterTraceListener()
+        {
+            try
+            {
+                if (Writer != null)
+                    Writer.Close();
+            }
+            catch
+            {
+                ;
+            }
+        }
+
+        public override void Write(string message)
+        {
+            try
+            {
+                Writer.Write("-> " + DateTime.Now.ToString() + "\t" + message);
+            }
+            catch (Exception exc)
+            {
+                Logger.E("Hata dosyasİna yazİlamAd:" + exc.Message);
+            }
+        }
+
+        public override void WriteLine(string message)
+        {
+            try
+            {
+                Writer.WriteLine("-> " + DateTime.Now.ToString() + "\t" + message);
+            }
+            catch (Exception exc)
+            {
+                Logger.E("Hata kaydedilemedi:" + exc.Message);
+            }
+        }
+    }
+}
