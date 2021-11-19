4ced6f6c353e2f3f6e3112af4779db6c7d77d816
Hüseyin Çelik <canavar.toro@gmail.com>
2021-11-19 23:28:14 +0300

    stok calismasi

--- OracleListener/Data/DataSynchronization.cs
+++ OracleListener/Data/DataSynchronization.cs
@@ -41,53 +41,14 @@ ORDER BY IT.ITEM_CODE");
             {
                 for (int i = 0; i < itemlist.Count; i++)
                 {
-                    var t = sqlClient.Count($@"SELECT COUNT(*) AS TX FROM dbo.F_ARTICLE WITH (NOLOCK) WHERE DE_Code = N'{itemlist[i].ITEM_CODE}'");
-                    if (t == 0)
-                    {
-                        SqlParameter[] parameters = new SqlParameter[24];
-                        parameters[0] = new SqlParameter("@P1", depolist[i].WHOUSE_ID);
-                        parameters[1] = new SqlParameter("@P2", depolist[i].WHOUSE_DESC.GetParamValue());
-                        parameters[2] = new SqlParameter("@P3", depolist[i].ADDRESS1.GetParamValue());
-                        parameters[3] = new SqlParameter("@P4", depolist[i].COMPLEMENT.GetParamValue());
-                        parameters[4] = new SqlParameter("@P5", depolist[i].POST_CODE.GetParamValue());
-                        parameters[5] = new SqlParameter("@P6", depolist[i].CITY_NAME.GetParamValue());
-                        parameters[6] = new SqlParameter("@P7", DBNull.Value); // DE_Contact     depo sorumlusu
-                        parameters[7] = new SqlParameter("@P8", 1);  //DE_Principal
-                        parameters[8] = new SqlParameter("@P9", 1);  //DE_CatCompta
-                        parameters[9] = new SqlParameter("@P10", DBNull.Value); // DE_Region
-                        parameters[10] = new SqlParameter("@P11", depolist[i].COUNTRY_NAME.GetParamValue());// DE_Pays
-                        parameters[11] = new SqlParameter("@P12", depolist[i].EMAIL.GetParamValue());
-                        parameters[12] = new SqlParameter("@P13", depolist[i].WHOUSE_CODE.GetParamValue());
-                        parameters[13] = new SqlParameter("@P14", depolist[i].PHONE.GetParamValue());
-                        parameters[14] = new SqlParameter("@P15", depolist[i].PHONE2.GetParamValue());
-                        parameters[15] = new SqlParameter("@P16", 1);//   DE_Replication
-                        parameters[16] = new SqlParameter("@P17", DBNull.Value); //   DP_NoDefaut
-                        parameters[17] = new SqlParameter("@P18", 1);//   cbDP_NoDefaut
-                        parameters[18] = new SqlParameter("@P19", 1);//   DE_Exclure
-                        parameters[19] = new SqlParameter("@P20", 1);//   DE_Souche01
-                        parameters[20] = new SqlParameter("@P21", 1);//   DE_Souche02
-                        parameters[21] = new SqlParameter("@P22", 1);//   DE_Souche03
-                        parameters[22] = new SqlParameter("@P23", AppConfig.Default.CreateUser.GetParamValue());//   cbCreateur
-                        parameters[23] = new SqlParameter("P24", AppConfig.Default.CreateUserId.GetParamValue());//   cbCreationUser
-                        var obj = sqlClient.ExecuteScalar($@"DECLARE @TableMarq TABLE (cbMarq int);
-INSERT INTO F_DEPOT (DE_No,DE_Intitule,DE_Adresse,DE_Complement,DE_CodePostal,DE_Ville,DE_Contact,DE_Principal,DE_CatCompta,DE_Region,DE_Pays,DE_EMail,DE_Code,DE_Telephone,DE_Telecopie,DE_Replication,DP_NoDefaut,cbDP_NoDefaut,DE_Exclure,DE_Souche01,DE_Souche02,DE_Souche03,cbCreateur,cbCreationUser) OUTPUT inserted.cbMarq INTO @TableMarq VALUES (@P1,@P2,@P3,@P4,@P5,@P6,@P7,@P8,@P9,@P10,@P11,@P12,@P13,@P14,@P15,@P16,@P17,@P18,@P19,@P20,@P21,@P22,@P23,@P24);
-SELECT cbMarq FROM @TableMarq;", parameters);
-                        if (obj != null)
-                        {
-                            Int16 dbtype = 0;
-                            parameters = new SqlParameter[8];
-                            parameters[0] = new SqlParameter("@P1", depolist[i].WHOUSE_ID);
-                            parameters[1] = new SqlParameter("@P2", depolist[i].WHOUSE_ID);
-                            parameters[2] = new SqlParameter("@P3", depolist[i].WHOUSE_CODE);
-                            parameters[3] = new SqlParameter("@P4", depolist[i].WHOUSE_DESC.GetParamValue());
-                            parameters[4] = new SqlParameter("@P5", DBNull.Value);//    DP_Zone
-                            parameters[5] = new SqlParameter("@P6", dbtype);//  DP_Type
-                            parameters[6] = new SqlParameter("@P7", AppConfig.Default.CreateUser.GetParamValue());//   cbCreateur
-                            parameters[7] = new SqlParameter("@P8", AppConfig.Default.CreateUserId.GetParamValue());//   cbCreationUser
-                            sqlClient.Execute($@"DECLARE @TableMarq TABLE (cbMarq int);
-INSERT INTO F_DEPOTEMPL (DE_No,DP_No,DP_Code,DP_Intitule,DP_Zone,DP_Type,cbCreateur,cbCreationUser) OUTPUT inserted.cbMarq INTO @TableMarq VALUES (@P1,@P2,@P3,@P4,@P5,@P6,@P7,@P8); SELECT cbMarq FROM @TableMarq;", parameters);
-                        }
-                    }
+                    SqlParameter[] parameters = new SqlParameter[5];
+                    parameters[0] = new SqlParameter("@ITEM_ID", itemlist[i].ITEM_ID);
+                    parameters[1] = new SqlParameter("@ITEM_CODE", itemlist[i].ITEM_CODE.GetParamValue());
+                    parameters[2] = new SqlParameter("@ITEM_NAME", itemlist[i].ITEM_NAME.GetParamValue());
+                    parameters[3] = new SqlParameter("@UNIT_ID", itemlist[i].UNIT_ID);
+                    parameters[4] = new SqlParameter("@UNIT_CODE", itemlist[i].UNIT_CODE.GetParamValue());
+
+                    sqlClient.Execute("EXECUTE dbo.CREATE_ARTICLE @ITEM_ID, @ITEM_CODE, @ITEM_NAME, @UNIT_ID, @UNIT_CODE", parameters);
                 }
             }
 
@@ -111,7 +72,14 @@ ORDER BY WH.WHOUSE_CODE");
             {
                 for (int i = 0; i < depolist.Count; i++)
                 {
-                    var t = sqlClient.Count($@"SELECT COUNT(*) AS TX FROM dbo.F_DEPOT WITH (NOLOCK) WHERE DE_Code = N'{depolist[i].WHOUSE_CODE}'");
+                    SqlParameter[] parameters = new SqlParameter[3];
+                    parameters[0] = new SqlParameter("@WHOUSE_ID", depolist[i].WHOUSE_ID);
+                    parameters[1] = new SqlParameter("@WHOUSE_CODE", depolist[i].WHOUSE_CODE.GetParamValue());
+                    parameters[2] = new SqlParameter("@DESCRIPTION", depolist[i].WHOUSE_DESC.GetParamValue());
+
+                    sqlClient.Execute("EXECUTE dbo.CREATE_DEPOT @WHOUSE_ID, @WHOUSE_CODE, @DESCRIPTION", parameters);
+
+                    /*var t = sqlClient.Count($@"SELECT COUNT(*) AS TX FROM dbo.F_DEPOT WITH (NOLOCK) WHERE DE_Code = N'{depolist[i].WHOUSE_CODE}'");
                     if (t == 0)
                     {
                         SqlParameter[] parameters = new SqlParameter[24];
@@ -157,7 +125,7 @@ SELECT cbMarq FROM @TableMarq;", parameters);
                             sqlClient.Execute($@"DECLARE @TableMarq TABLE (cbMarq int);
 INSERT INTO F_DEPOTEMPL (DE_No,DP_No,DP_Code,DP_Intitule,DP_Zone,DP_Type,cbCreateur,cbCreationUser) OUTPUT inserted.cbMarq INTO @TableMarq VALUES (@P1,@P2,@P3,@P4,@P5,@P6,@P7,@P8); SELECT cbMarq FROM @TableMarq;", parameters);
                         }
-                    }
+                    }*/
                 }
             }
 
---
+++ OracleListener/Scripts/CREATE_DEPOT.sql
@@ -0,0 +1,54 @@
+﻿
+ALTER PROCEDURE dbo.CREATE_DEPOT(
+	@WHOUSE_ID INTEGER,
+	@WHOUSE_CODE NVARCHAR(60),
+	@DESCRIPTION NVARCHAR(150)
+)
+AS BEGIN
+DECLARE
+@DE_No int = @WHOUSE_ID,
+@DE_Intitule varchar(35) = @DESCRIPTION,
+@DE_Adresse varchar(35) = NULL,
+@DE_Complement varchar(35) = NULL,
+@DE_CodePostal varchar(9) = NULL,
+@DE_Ville varchar(35) = NULL,
+@DE_Contact varchar(35) = NULL,
+@DE_Principal smallint = 1,
+@DE_CatCompta smallint = 1,
+@DE_Region varchar(25) = NULL,
+@DE_Pays varchar(35) = N'Côte d''Ivoire',
+@DE_EMail varchar(69) = NULL,
+@DE_Code varchar(9) = @WHOUSE_CODE,
+@DE_Telephone varchar(21) = NULL,
+@DE_Telecopie varchar(21) = NULL,
+@DE_Replication int = 0,
+@DP_NoDefaut int = NULL,
+@cbDP_NoDefaut int = NULL,
+@DE_Exclure smallint = 0,
+@DE_Souche01 smallint = 0,
+@DE_Souche02 smallint = 0,
+@DE_Souche03 smallint = 0,
+@cbProt smallint = NULL,
+@cbMarq int = NULL,
+@cbCreateur char(4) = N'ERP1',
+@cbModification datetime = NULL,
+@cbReplication int = NULL,
+@cbFlag smallint = NULL,
+@cbCreation datetime = GETDATE(),
+@cbCreationUser uniqueidentifier = '77384016-921F-472F-B56D-1D563B7DDF3C'
+
+IF EXISTS (SELECT * FROM dbo.F_DEPOT WITH (NOLOCK) WHERE DE_Code = @DE_Code) BEGIN
+	UPDATE dbo.F_DEPOT SET DE_Intitule = @DE_Intitule WHERE DE_Code = @DE_Code
+END ELSE BEGIN
+	DECLARE @TableMarq TABLE (cbMarq int);
+	INSERT INTO F_DEPOT (DE_No,DE_Intitule,DE_Adresse,DE_Complement,DE_CodePostal,DE_Ville,DE_Contact,DE_Principal,DE_CatCompta,DE_Region,DE_Pays,DE_EMail,DE_Code,DE_Telephone,DE_Telecopie,DE_Replication,DP_NoDefaut,cbDP_NoDefaut,DE_Exclure,DE_Souche01,DE_Souche02,DE_Souche03,cbCreateur,cbCreationUser) OUTPUT inserted.cbMarq INTO @TableMarq 
+	VALUES (@DE_No,@DE_Intitule,@DE_Adresse,@DE_Complement,@DE_CodePostal,@DE_Ville,@DE_Contact,@DE_Principal,@DE_CatCompta,@DE_Region,@DE_Pays,@DE_EMail,@DE_Code,@DE_Telephone,@DE_Telecopie,@DE_Replication,@DP_NoDefaut,@cbDP_NoDefaut,@DE_Exclure,@DE_Souche01,@DE_Souche02,@DE_Souche03,@cbCreateur,@cbCreationUser);
+	SELECT @cbMarq = cbMarq FROM @TableMarq;
+
+	INSERT INTO F_DEPOTEMPL (DE_No,DP_No,DP_Code,DP_Intitule,DP_Zone,DP_Type,cbCreateur,cbCreationUser) --OUTPUT inserted.cbMarq INTO @TableMarq 
+	VALUES (@DE_No,@DE_No,@DE_Code,@DE_Intitule,1,0,@cbCreateur,@cbCreationUser); 
+	--SELECT cbMarq FROM @TableMarq;
+
+END
+
+END
