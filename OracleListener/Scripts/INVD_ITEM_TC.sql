97222dfb5349a0de6b0eca66b1aba6ec88e42427
Hüseyin Çelik <canavar.toro@gmail.com>
2021-11-27 10:42:42 +0300

    trigger

--- OracleListener/Scripts/INVD_ITEM_TC.sql
+++ OracleListener/Scripts/INVD_ITEM_TC.sql
@@ -1,5 +1,5 @@
-﻿CREATE OR REPLACE TRIGGER INVBRANCH_WHOUSE_TC
-AFTER INSERT OR UPDATE ON UYUMSOFT.INVD_BRANCH_WHOUSE
+﻿CREATE OR REPLACE TRIGGER INVD_ITEM_TC
+AFTER INSERT OR UPDATE ON UYUMSOFT.INVD_ITEM
 FOR EACH ROW
 DECLARE
   c  utl_tcp.connection;  -- TCP/IP connection to the Web server
@@ -9,14 +9,14 @@ BEGIN
   c := utl_tcp.open_connection(remote_host => '10.225.0.202',
                                remote_port =>  4444,
                                charset     => 'US7ASCII');  -- open connection
-  ret_val := utl_tcp.write_line(c, :NEW.WHOUSE_ID || '|DEPO');    -- send HTTP request
+  ret_val := utl_tcp.write_line(c, :NEW.ITEM_ID || '|STOK');    -- send HTTP request
    UTL_TCP.flush (c);
   
   utl_tcp.close_connection(c);
-
+  
 EXCEPTION
     WHEN OTHERS THEN
     NULL;
 
-END INVBRANCH_WHOUSE_TC;
+END INVD_ITEM_TC;
 /
