4870e7f1a971142dbb0dc3012a87b9a7caeeef56
Hüseyin Çelik <canavar.toro@gmail.com>
2022-05-23 21:32:45 +0300

    calisma

--- OracleListener/Form1.cs
+++ OracleListener/Form1.cs
@@ -131,6 +131,7 @@ ORDER BY WH.WHOUSE_CODE");
         {
             Task.Run(() =>
             {
+                Logger.I($"{Environment.GetFolderPath(Environment.SpecialFolder.Templates)}\\CheckItemMField.log");
                 if (!File.Exists($"{Environment.GetFolderPath(Environment.SpecialFolder.Templates)}\\CheckItemMField.log"))
                 {
                     using (OracleProvider db = new OracleProvider())
@@ -144,15 +145,13 @@ ORDER BY WH.WHOUSE_CODE");
                         var tblcount = db.ExecuteScalar($"SELECT COUNT(*) TB_COUNT FROM ALL_TABLES WHERE TABLE_NAME = 'ZFIND_SAGE_HESAPPLANI'");
                         if (tblcount == null || Convert.ToInt32(tblcount) == 0)
                         {
-                            if (db.Execute(@"CREATE SEQUENCE  ""UYUMSOFT"".""ZFIND_SAGE_HESAPPLANI_ID""  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE"))
-                            {
-                                db.Execute(@"CREATE TABLE ""UYUMSOFT"".""ZFIND_SAGE_HESAPPLANI"" (
+                            db.Execute(@"CREATE SEQUENCE  ""UYUMSOFT"".""ZFIND_SAGE_HESAPPLANI_ID""  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE");
+                            db.Execute(@"CREATE TABLE ""UYUMSOFT"".""ZFIND_SAGE_HESAPPLANI"" (
   ""HESAPPLANI_ID"" NUMBER NOT NULL,
   ""ACC_CODE"" VARCHAR2(25) NOT NULL,
   ""HESAPPLANI_CODE"" VARCHAR2(50) NOT NULL,
   PRIMARY KEY(""HESAPPLANI_ID"")
 )");
-                            }
                         }
                     }
                     File.WriteAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.Templates)}\\CheckItemMField.log", DateTime.Now.ToString());
---
+++ OracleListener/Scripts/izin.sql
@@ -0,0 +1,48 @@
+﻿--sample, şifre:edirsel
+DECLARE
+  c  utl_tcp.connection;  -- TCP/IP connection to the Web server
+  ret_val pls_integer; 
+BEGIN
+  c := utl_tcp.open_connection(remote_host => '127.0.0.1',
+                               remote_port =>  4444,
+                               charset     => 'US7ASCII');  -- open connection
+  ret_val := utl_tcp.write_line(c, '3151|DEPO');    -- send HTTP request
+   UTL_TCP.flush (c);
+  
+  utl_tcp.close_connection(c);
+END;
+
+
+
+CREATE OR REPLACE TRIGGER UYUMSOFT.INVBRANCH_WHOUSE_TC
+AFTER INSERT OR UPDATE ON UYUMSOFT.INVD_BRANCH_WHOUSE
+FOR EACH ROW
+DECLARE
+  c  utl_tcp.connection;  -- TCP/IP connection to the Web server
+  ret_val pls_integer; 
+BEGIN
+
+  c := utl_tcp.open_connection(remote_host => '10.225.0.202',
+                               remote_port =>  4444,
+                               charset     => 'US7ASCII');  -- open connection
+  ret_val := utl_tcp.write_line(c, :NEW.WHOUSE_ID || '|DEPO');    -- send HTTP request
+   UTL_TCP.flush (c);
+  
+  utl_tcp.close_connection(c);
+
+
+
+END INVBRANCH_WHOUSE_TC;
+/
+
+
+
+grant  execute on UTL_TCP to uyumsoft;
+
+grant  execute on UTL_SMTP to uyumsoft;
+
+grant  execute on UTL_MAIL to uyumsoft;
+
+grant  execute on UTL_HTTP to uyumsoft;
+
+grant  execute on UTL_INADDR to uyumsoft;
